3a9fed8bc2c0da7dc8936a9aa1340050f12556b7
Javier Rosales <cyjav.dev@gmail.com>
2022-11-21 19:14:13 -0600

    Resolve 02_repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(text, repeatTimes) {
+    let output = ""
 
+    if (repeatTimes < 0) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < repeatTimes; i++) {
+        output += text
+    }
+
+    return output
 };
 
 // Do not edit below this line

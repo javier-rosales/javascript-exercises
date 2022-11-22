a50b2c4f0ff942cc8485c2d498857224e2931aa4
Javier Rosales <cyjav.dev@gmail.com>
2022-11-22 12:58:33 -0600

    Resolve 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let result = 0
 
+    if (start < 0 ||
+        end < 0 ||
+        typeof start !== "number" ||
+        typeof end !== "number") {
+        return "ERROR"
+    } else if (start === end) {
+        return start
+    } else if (start > end) {
+        for (let i = start; i >= end; i--) {
+            result += i
+        }
+    } else {
+        for (let i = start; i <= end; i++) {
+            result += i
+        }
+    }
+
+    return result
 };
 
 // Do not edit below this line

b841d6e862dd8c9b92bd7858b266b9599c605d05
Javier Rosales <cyjav.dev@gmail.com>
2022-11-21 23:24:11 -0600

    Resolve 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...items) {
+    if (items.length === 0) {
+        return list
+    }
 
+    for (const item of items) {
+        itemIndex = list.indexOf(item)
+        if (itemIndex !== -1) {
+            list.splice(itemIndex, 1)
+        }
+    }
+
+    return list
 };
 
 // Do not edit below this line

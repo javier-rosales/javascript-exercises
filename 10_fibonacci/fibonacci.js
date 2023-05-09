76bbecdadcba7f6ba8009d3e08f50a256739b92e
Javier Rosales <cyjav.dev@gmail.com>
2023-05-09 11:29:17 -0600

    Resolve 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 1) return "OOPS"
+    const sequence = []
+    for (let count = 0; count < member; count++) {
+        sequence.push(getMember(sequence))
+    }
+    return sequence[sequence.length - 1]
+}
 
-};
+function getMember(sequence) {
+    const length = sequence.length
+    if (length === 0 || length === 1) {
+        return 1
+    }
+    return sequence[length - 1] + sequence[length - 2]
+}
 
 // Do not edit below this line
 module.exports = fibonacci;

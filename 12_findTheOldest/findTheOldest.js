5635fad64f40c16daf657fbfdfe66041661b16ca
Javier Rosales <cyjav.dev@gmail.com>
2023-05-09 14:09:55 -0600

    Resolve 12_findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.forEach(person => {
+        if (!("yearOfDeath" in person)) {
+            const date = new Date()
+            person.yearsLived = date.getFullYear() - person.yearOfBirth
+        } else {
+            person.yearsLived = person.yearOfDeath - person.yearOfBirth
+        }
+    })
+    people.sort((a, b) => (a.yearsLived < b.yearsLived) ? 1 : -1)
+    return people[0]
 };
 
 // Do not edit below this line

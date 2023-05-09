f048b7260353d35cf95ba01be78637aa9df32e73
Javier Rosales <cyjav.dev@gmail.com>
2023-05-08 21:55:41 -0600

    Resolve 08_calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+	return number1 + number2
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1 - number2
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let result = 0
+  numbers.forEach(number => result += number)
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let result = 1
+  numbers.forEach(number => result *= number)
+  return result
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1
+  let result = number
+  for(number; number > 1; number--) {
+    result *= number - 1
+  }
+  return result
 };
 
 // Do not edit below this line

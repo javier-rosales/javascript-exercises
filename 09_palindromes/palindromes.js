6b45c3bb17014c4e3c5215fb7e4add72a3f90c20
Javier Rosales <cyjav.dev@gmail.com>
2023-05-08 23:24:35 -0600

    Resolve 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const alphabet = "abcdefghijklmnoprstuvwxzy"
+    
+    const clearedText = text
+                        .toLowerCase()
+                        .split("")
+                        .filter(letter => alphabet.includes(letter))
+                        .join("")
+    
+    const reversedText = clearedText
+                         .split("")
+                         .reverse()
+                         .join("")
+    
+    return clearedText === reversedText
 };
 
 // Do not edit below this line
